1e3cd3502e5e8a61e1c14932444cbc5e0bcd944e
oh <sjoh@inzent.com>
2021-06-03 17:00:47 +0900

    Initial commit

---
+++ src/main/java/com/sjoh/sjoh/SjohApplication.java
@@ -0,0 +1,13 @@
+package com.sjoh.sjoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjohApplication.class, args);
+	}
+
+}
